3cd4d18f85d9490262b58c00f151b1df205521b5
ryoiichi <40713976+ryoiichi@users.noreply.github.com>
2018-07-09 04:14:00 +0900

    db validation

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from sample.models import lesson
 
 # Create your views here.
 def index(request):
-    #とりあえず文字列を返す。
-    return HttpResponse("Indexです！")
+    lessons = lesson.objects.all().order_by('id') #値を取得
+    return render(request, 'sample/index.html', {'lessons':lessons}) #Templateに値を渡す
